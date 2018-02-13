5452ece7b4144c67ba0854ae8b8e225ed19ec0f7
rocket robin <jbl-2011@163.com>
2018-02-13 17:17:53 +0800

    create stylecop.json link

--- Myrmec.Test/MyrmecExtentionsTest.cs
+++ Myrmec.Test/MyrmecExtentionsTest.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Myrmec.Test
 {
