ff4c8560d42179e47fa0f0880b4a7d2b44379f3a
rocket robin <jbl-2011@163.com>
2018-06-08 17:14:35 +0800

    add MimeTypesTest

---
+++ Myrmec.Test/MimeTypesTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myrmec.Test
+{
+    [TestClass]
+    public class MimeTypesTest
+    {
+        [TestMethod]
+        public void DotTest()
+        {
+            var extension = ".jpg";
+            var result = MimeTypes.GetMimeType(extension);
+            Assert.AreEqual<string>("image/jpeg", result);
+        }
+
+        [TestMethod]
+        public void RegularTest()
+        {
+            var extension = "jpg";
+            var result = MimeTypes.GetMimeType(extension);
+            Assert.AreEqual<string>("image/jpeg", result);
+        }
+
+    }
+}
